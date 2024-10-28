f853d6f444f997d8223ed9e78fed308ba4e15385
calleha <calleha@disroot.org>
2024-10-28 17:55:21 +0100

    modified:   .local/bin/setkeyboardlayout.sh

--- .local/bin/setkeyboardlayout.sh
+++ .local/bin/setkeyboardlayout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-LAYOUTS="us\nus -variant altgr-intl\nus -variant intl\nse\nfr\nel -variant polytonic"
+LAYOUTS="us\nus -variant altgr-intl\nus -variant intl\nse\nfr\nel -variant polytonic\nes -variant cat\nara -variant buckwalter"
 echo -e $LAYOUTS | dmenu -b -p "choose layout" | xargs setxkbmap -option ctrl:nocaps -layout
